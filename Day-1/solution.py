4a25ae248596a062dacb8dfab0d774c759009913
Yuehua Yin <yy.yin02@gmail.com>
2022-12-01 19:11:29 +0000

    updated

--- Day-1/solution.py
+++ Day-1/solution.py
@@ -1,10 +1,8 @@
 f = open("input.txt", "r")
 counts = []
 count = 0
-deer = 0
 for line in f.readlines():
     if line == "\n":
-        deer += 1
         counts.append(count)
         count = 0
     else:
