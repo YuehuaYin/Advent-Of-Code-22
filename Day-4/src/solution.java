a075cafc888682a629828b64ee20c59b857e8673
Yuehua Yin <yy.yin02@gmail.com>
2022-12-04 19:11:04 +0000

    day 4 solution

--- Day-4/src/solution.java
+++ Day-4/src/solution.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class solution {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class solution {
+    public static void main(String[] args) throws FileNotFoundException {
+        int ans1 = 0;
+        int ans2 = 0;
+        BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
+        try {
+            while (reader.ready()) {
+                String [] line = reader.readLine().split(",|-");
+                if (Integer.parseInt(line[0]) <= Integer.parseInt(line[2]) && Integer.parseInt(line[1]) >= Integer.parseInt(line[3])){
+                    ans1 += 1;
+                    ans2 += 1;
+                }
+                else if (Integer.parseInt(line[0]) >= Integer.parseInt(line[2]) && Integer.parseInt(line[1]) <= Integer.parseInt(line[3])){
+                    ans1 += 1;
+                    ans2 += 1;
+                }
+                else if (Integer.parseInt(line[0]) <= Integer.parseInt(line[2]) && Integer.parseInt(line[2]) <= Integer.parseInt(line[1])){
+                    ans2 += 1;
+                }
+                else if (Integer.parseInt(line[2]) <= Integer.parseInt(line[0]) && Integer.parseInt(line[0]) <= Integer.parseInt(line[3])){
+                    ans2 += 1;
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println(ans1);
+        System.out.println(ans2);
+    }
 }
